ac37d87ae2b05f8c5b8181253fd8d31499ec665d
V-run64 <vpandian4u@gmail.com>
2025-10-05 04:34:59 -0400

    target score display and mousetrap

--- Object/main.gd
+++ Object/main.gd
@@ -2,13 +2,27 @@ extends Node2D
 
 var score: int
 var high_score= 0
-var target_score = 200
+var target_score: int
 var prev_score: int
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Timer.start()
+	target_score = _get_target_score()
+	
+	
+	
+	
+func _get_target_score() -> int:
+	var target := 0
+	for pickup in get_tree().get_nodes_in_group("cheese"):
+		if pickup.has_method("get_score_value"):
+			target += pickup.get_score_value()
+	return target - (target / 10)
+	
+	
+	
 	
 
 
@@ -16,7 +30,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var rounded = "%.2f" % $Timer.time_left
 	$TimerLabel.text = str(rounded)
-	$ScoreLabel.text = str(score)
+	$ScoreLabel.text = "Target Score: " + str(target_score)
 
 
 func _on_timer_timeout() -> void:
--- collision_shape_2d.gd
+++ collision_shape_2d.gd
@@ -3,8 +3,12 @@ extends CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("mouse"):
+		print("ded")
+		body.queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
